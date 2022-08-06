c1d3fcb69ddbeaa50f422a3be1c9ecc51cbd1841
Maxim Nazarov <xonskyfilms@gmail.com>
2022-08-06 13:35:20 +0300

    Function

--- src/index.js
+++ src/index.js
@@ -2,11 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+let likes = 0;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root'));
+
 root.render(
  
     <div>
-      <h1>Приложение рабоает!</h1>
+      <div className='likes'>{likes}</div>
+      <button onClick={addLike()} className='btn-like'>Like</button>
+      <button onClick={disLike()} className='btn-dislike'>Dislike</button>
     </div>
 );
+
+
+function addLike(){
+  likes += 1
+  console.log(likes)
+}
+
+function disLike(){
+  likes -= 1
+  console.log(likes)
+}
+
+
