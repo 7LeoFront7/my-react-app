79433f9eaafd5f46956852df610041973ea8be72
Maxim Nazarov <xonskyfilms@gmail.com>
2022-08-06 13:49:21 +0300

    More fun

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function App() {
+function App(){
+  const [likes, setLikes] = useState(5)
+  const [value, setValue] = useState('TEXT IN INPUT')
+
+
+
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <h1>{likes}</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
     </div>
-  );
+  )
+
+
+  function increment(){
+    setLikes(likes + 1)
+  }
+
+  function decrement(){
+    setLikes(likes - 1)
+  }
+
+function addLike(){
+  likes += 1
+  console.log(likes)
+}
+
+function disLike(){
+  likes -= 1
+  console.log(likes)
+}
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,32 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-let likes = 0;
 
 
-
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root'));
-
-root.render(
- 
-    <div>
-      <div className='likes'>{likes}</div>
-      <button onClick={addLike()} className='btn-like'>Like</button>
-      <button onClick={disLike()} className='btn-dislike'>Dislike</button>
-    </div>
-);
-
-
-function addLike(){
-  likes += 1
-  console.log(likes)
-}
-
-function disLike(){
-  likes -= 1
-  console.log(likes)
-}
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+)
 
 
